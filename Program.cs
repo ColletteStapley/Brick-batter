04c2642c22d7bf9ef61753b2a8977a21bd92c45c
Collette Stapley <sta18019@byui.edu>
2021-11-19 19:15:35 -0700

    Ball, brick, movve actor and part of handle actor

--- Program.cs
+++ Program.cs
@@ -17,11 +17,24 @@ namespace cse210_batter_csharp
             cast["bricks"] = new List<Actor>();
 
             // TODO: Add your bricks here
+            for (int y = 25; y < 160; y += (Constants.BRICK_HEIGHT + Constants.BRICK_SPACE))
+            {
+                for (int x = 30; x < 750; x += (Constants.BRICK_WIDTH + Constants.BRICK_SPACE))
+                {
+                    Brick b = new Brick();
+                    b.SetPosition(new Point(x, y));
+                    cast["bricks"].Add(b);
+                }
+            }
 
             // The Ball (or balls if desired)
             cast["balls"] = new List<Actor>();
 
             // TODO: Add your ball here
+            Ball orb = new Ball();
+            orb.SetPosition(new Point(400, 300));
+            cast["balls"].Add(orb);
+            
 
             // The paddle
             cast["paddle"] = new List<Actor>();
---
+++ Scripting/HandleCollisionsAction.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using cse210_batter_csharp.Casting;
+
+namespace cse210_batter_csharp.Scripting
+{
+    public class HandleCollisionsActions : Action
+    {
+        public HandleCollisionsActions()
+        {
+
+        }
+
+        public override void Execute(Dictionary<string, List<Actor>> cast)
+        {
+
+        }
+    }
+}
